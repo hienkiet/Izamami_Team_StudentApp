de1fd64fbc9b4557306ead90dd64238350504048
hienkiet <hienkietlamson@gmail.com>
2021-12-12 14:12:35 +0700

    add homepage, register, add students, bootsrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  resources :students
+  resources :users, :only =>[:show]
+  match '/users',   to: 'users#index',   via: 'get'
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  devise_for :users, :path_prefix => 'd'
+  get 'home/about'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
