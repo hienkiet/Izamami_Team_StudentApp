61b045ffb5a4f296ba55de7187399c7599572446
UongMinhTan <minhtan471100@gmail.com>
2021-12-13 22:56:19 +0700

    style: add btn to scores

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(version: 2021_12_13_141232) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_name"
     t.integer "user_age"
+    t.index "\"name\"", name: "index_users_on_name", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
